73b7b6f7083c860e94568fd723cd7d1852c0fbc7
AlirezaGhgh021 <alirgh.1919@gmail.com>
2025-11-25 15:52:39 +0330

    feat: initial FastAPI project with proper structure and potry

---
+++ src/jobboard_api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from jobboard_api.core.config import settings
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version="0.1.0",
+    debug=settings.DEBUG
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to your future job board API! Ready to rule FastAPI?"}
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy"}
